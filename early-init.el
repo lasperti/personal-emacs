c3efc60f87674160bd0e77a366c4c802c3a4fe22
Lucas Asperti <git@lasperti.com>
2026-07-05 19:44:20 -0300

    feat: initial commit

---
+++ early-init.el
@@ -0,0 +1,11 @@
+;;; early-init.el --- the small one -*- lexical-binding: t -*-
+
+;; Author: lasperti
+
+;;; Commentary:
+
+;; Simple early-init.el to maximize speed and UI minimalism.
+
+;;; Code:
+
+;;; early-init.el ends here
---
+++ init.el
@@ -0,0 +1,13 @@
+;;; init.el --- the big one -*- lexical-binding: t -*-
+
+;; Author: lasperti
+
+;;; Commentary:
+
+;; This is my personal Emacs configuration.
+;; I do everything inside Emacs. It is my favourite piece of software.
+;; Reading this code ends up being reading a part of my brain too.
+
+;;; Code:
+
+;;; init.el ends here
