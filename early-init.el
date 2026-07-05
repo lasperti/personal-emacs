bf294d902f9f4f501deb9cf869f01eb59b5c3428
Lucas Asperti <git@lasperti.com>
2026-07-05 19:58:19 -0300

    feat: add ui purge

--- early-init.el
+++ early-init.el
@@ -9,4 +9,19 @@
 ;;; Code:
 (setq package-enable-at-startup nil)
 
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars . nil) default-frame-alist)
+
+(setq frame-inhibit-implied-resize t)
+(setq inhibit-x-resources t)
+
+(setq ring-bell-function 'ignore)
+
+(setq inhibit-startup-screen t
+      inhibit-startup-echo-area-message user-login-name)
+
+(push '(background-color . "#ffffff") default-frame-alist)
+(push '(foreground-color . "#000000") default-frame-alist)
+
 ;;; early-init.el ends here
