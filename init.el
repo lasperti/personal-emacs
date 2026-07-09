abbaab401aaca7a83ad7c4fe0823f5d021274037
Lucas Asperti <git@lasperti.com>
2026-07-09 14:25:32 -0300

    feat: add initial org-roam configuration

--- init.el
+++ init.el
@@ -73,4 +73,20 @@
   :hook
   (org-mode . visual-line-mode))
 
+;;;; org-roam
+(use-package org-roam
+  :custom
+  (org-roam-directory (file-truename "~/org"))
+  (org-roam-completion-everywhere t)
+
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)     
+         ("C-c n i" . org-roam-node-insert)   
+         ("C-c n c" . org-roam-capture)       
+         :map org-mode-map
+         ("C-M-i" . completion-at-point))
+
+  :config
+  (org-roam-db-autosync-mode))
+ 
 ;;; init.el ends here
